cc4684ab5ae45d62d21be904e73aeab85d46a26a
Alison Clarke <alison.r.clarke@durham.ac.uk>
2021-10-04 13:19:49 +0100

    Allow convert_history_to_python to be run from QGIS

--- scripts/qgis/processing_history/README.md
+++ scripts/qgis/processing_history/README.md
@@ -1,42 +1,68 @@
 # Turn processing history into an executable python script
 
-To export QGIS processing history based on a given data directory:
+`convert_history_to_python.py` looks in your QGIS processing log for processing commands run in a given data directory,
+and exports them as an executable file. This file can then be pushed to github or included in a data deposit, to enable
+others to reproduce your analysis.
 
-```
-python convert_history_to_python.py <data_dir>
-```
+## Exporting history
 
-e.g. if data is stored in `/Users/ksvf48/Documents/dev/pyqgis_in_a_day`
+To export QGIS processing history based on a given data directory (do **either** 1 **or** 2):
 
-```
-python convert_history_to_python.py /Users/ksvf48/Documents/dev/pyqgis_in_a_day
-```
+1.  **From the command line, if you have python installed**:
 
-This exports a file `qgis_commands.py` in the current working directory which contains all the qGIS processing done on files within that folder.
+    Change directory to the location of `convert_history_to_python`, (e.g. `/Users/ksvf48/Documents/GitHub/domain-specific-software-sustainability/scripts/qgis/processing_history`) and run:
+
+    ```bash
+    python convert_history_to_python.py <data_dir>
+    ```
+
+    e.g. if data is stored in `/Users/ksvf48/Documents/dev/pyqgis_in_a_day`
+
+    ```bash
+    python convert_history_to_python.py /Users/ksvf48/Documents/dev/pyqgis_in_a_day
+    ```
+
+2.  **From QGIS**:
+
+    a. Open the python console.
+    b. Type the following, replacing `<location>` with the location where this directory is checked out, e.g. `sys.path.append('/Users/ksvf48/Documents/GitHub/domain-specific-software-sustainability/scripts/qgis/processing_history')`, and `<data_dir>` with the directory where your input data is stored:
+
+    ```python
+    sys.path.append('<location>')
+    import convert_history_to_python
+    convert_history_to_python.convert_history_to_python('<data_dir>')
+    ```
+
+`convert_history_to_python` exports a file `qgis_commands.py` in the current working directory which contains all the qGIS processing done on files within that folder.
 
 e.g.
 
-```
+```python
 from qgis import processing
+from qgis.core import QgsMessageLog
 
-def run(data_dir):
+def run(data_dir='/Users/ksvf48/Downloads/packages/Natural_Earth_quick_start/'):
     # 2021-08-12 16:20:38
-    processing.run("qgis:exportaddgeometrycolumns", {'INPUT':'{0}/110m_physical/ne_110m_rivers_lake_centerlines.shp|layerid=0|subset=\"min_zoom\" <= 0'.format(data_dir),'CALC_METHOD':2,'OUTPUT':'TEMPORARY_OUTPUT'})
+    QgsMessageLog.logMessage("Running command: processing.run(\"qgis:exportaddgeometrycolumns\", {'INPUT':'{0}110m_physical/ne_110m_rivers_lake_centerlines.shp|layerid=0|subset=\"min_zoom\" <= 0'.format(data_dir),'CALC_METHOD':2,'OUTPUT':'TEMPORARY_OUTPUT'})")
+    processing.run("qgis:exportaddgeometrycolumns", {'INPUT':'{0}110m_physical/ne_110m_rivers_lake_centerlines.shp|layerid=0|subset=\"min_zoom\" <= 0'.format(data_dir),'CALC_METHOD':2,'OUTPUT':'TEMPORARY_OUTPUT'})
     # 2021-08-12 16:23:55
-    processing.run("qgis:exportaddgeometrycolumns", {'INPUT':'{0}/10m_cultural/ne_10m_roads.shp|layerid=0|subset=\"min_zoom\" <= 5'.format(data_dir),'CALC_METHOD':0,'OUTPUT':'TEMPORARY_OUTPUT'})
+    QgsMessageLog.logMessage("Running command: processing.run(\"qgis:exportaddgeometrycolumns\", {'INPUT':'{0}10m_cultural/ne_10m_roads.shp|layerid=0|subset=\"min_zoom\" <= 5'.format(data_dir),'CALC_METHOD':0,'OUTPUT':'TEMPORARY_OUTPUT'})")
+    processing.run("qgis:exportaddgeometrycolumns", {'INPUT':'{0}10m_cultural/ne_10m_roads.shp|layerid=0|subset=\"min_zoom\" <= 5'.format(data_dir),'CALC_METHOD':0,'OUTPUT':'TEMPORARY_OUTPUT'})
 ```
 
-If necessary, this file could be edited by hand to remove duplicate entries (e.g. if an analysis was retried with differenr parameters).
+If necessary, this file could be edited by hand to remove duplicate entries (e.g. if an analysis was retried with different parameters).
 
 The exported file could then be stored in version control.
 
-To rerun the processing from qGIS:
+## Running the exported script
+
+To rerun the processing from QGIS:
 
 1. Open the python console.
 2. Run the following commands, replacing `<script_dir>` with the folder where `qgis_commands.py` is (e.g. `/Users/ksvf48/Documents/dev/domain-specific-software-sustainability/scripts/qgis/processing`):
 
   ```
-  cd '<script_dir>'
+  sys.path.append('<script_dir>')
   import qgis_commands
   qgis_commands.run()
   ```
--- scripts/qgis/processing_history/convert_history_to_python.py
+++ scripts/qgis/processing_history/convert_history_to_python.py
@@ -2,69 +2,82 @@ import os
 import re
 import sys
 
-if len(sys.argv) < 2:
-    print("No input data dir specified - exiting")
-    sys.exit(1)
 
-data_dir = sys.argv[1]
-
-if len(sys.argv) > 2:
-    qgis_user_profile_dir = os.path.realpath(sys.argv[2])
-else:
-    if sys.platform.startswith('win32'):
-        qgis_user_profile_dir = os.path.join(
-            os.environ.get('APPDATA'),
-            'QGIS/QGIS3/profiles/default'
-        )
-        if not os.path.isdir(qgis_user_profile_dir):
+def convert_history_to_python(data_dir, qgis_user_profile_dir=None):
+    if qgis_user_profile_dir is None:
+        if sys.platform.startswith('win32'):
             qgis_user_profile_dir = os.path.join(
-                os.environ.get('LOCALAPPDATA'),
+                os.environ.get('APPDATA'),
                 'QGIS/QGIS3/profiles/default'
             )
-    elif sys.platform.startswith('darwin'):
-        qgis_user_profile_dir = os.path.join(
-            '/Users', os.environ.get('USER'),
-            'Library/Application Support/QGIS/QGIS3/profiles/default'
-        )
+            if not os.path.isdir(qgis_user_profile_dir):
+                qgis_user_profile_dir = os.path.join(
+                    os.environ.get('LOCALAPPDATA'),
+                    'QGIS/QGIS3/profiles/default'
+                )
+        elif sys.platform.startswith('darwin'):
+            qgis_user_profile_dir = os.path.join(
+                '/Users', os.environ.get('USER'),
+                'Library/Application Support/QGIS/QGIS3/profiles/default'
+            )
 
+    processing_log = os.path.join(qgis_user_profile_dir, 'processing/processing.log')
 
-processing_log = os.path.join(qgis_user_profile_dir, 'processing/processing.log')
+    if not os.path.isfile(processing_log):
+        print("Could not find processing log at ", processing_log)
+        print("Please specify your QGIS Active Profile Folder as the second argument to this script.")
+        print("(In QGIS go to 'Settings' > 'User Profiles' > 'Open Active Profile Folder' and copy the path.)")
+        sys.exit(2)
 
-if not os.path.isfile(processing_log):
-    print("Could not find processing log at ", processing_log)
-    print("Please specify your QGIS Active Profile Folder as the second argument to this script.")
-    print("(In QGIS go to 'Settings' > 'User Profiles' > 'Open Active Profile Folder' and copy the path.)")
-    sys.exit(2)
+    with open(processing_log) as f:
+        lines = f.readlines()
 
-with open(processing_log) as f:
-    lines = f.readlines()
+    data_dir_regex = re.compile(r"'([^']*){0}([^']*)'".format(data_dir))
+    data_dir_replace_pattern = r"'\1{0}\2'.format(data_dir)"
 
-data_dir_regex = re.compile(r"'([^']*){0}([^']*)'".format(data_dir))
-data_dir_replace_pattern = r"'\1{0}\2'.format(data_dir)"
-
-output_file = 'qgis_commands.py'
-with open(output_file, 'w') as output:
-    output.write(f"""
+    # FIXME: add this script directory (for now) or output_dir
+    output_file = os.path.dirname(os.path.abspath(__file__)) + '/qgis_commands.py'
+    with open(output_file, 'w') as output:
+        output.write(f"""
 from qgis import processing
+
 
 def run(data_dir='{data_dir}'):
 """)
-    added_line = False
-    for line in lines:
-        if '|' in line:
-            # line example:
-            # ALGORITHM|~|2021-08-19 15:01:44|~|processing.run("native:hillshade", {'INPUT':'/Users/ksvf48/Documents/dev/pyqgis_in_a_day/srtm.tif','Z_FACTOR':1,'AZIMUTH':300,'V_ANGLE':40,'OUTPUT':'TEMPORARY_OUTPUT'})
-            parts = line.strip().split('|', maxsplit=4)
-            cmd = parts[4]
-            if data_dir_regex.search(cmd):
-                date = parts[2]
-                cmd2 = data_dir_regex.sub(data_dir_replace_pattern, cmd)
-                output.write(f'    # {date}\n')
-                output.write(f'    {cmd2}\n')
-                added_line = True
+        added_line = False
+        for line in lines:
+            if '|' in line:
+                print('--------------------------------')
+                # line example:
+                # ALGORITHM|~|2021-08-19 15:01:44|~|processing.run("native:hillshade", {'INPUT':'/Users/ksvf48/Documents/dev/pyqgis_in_a_day/srtm.tif','Z_FACTOR':1,'AZIMUTH':300,'V_ANGLE':40,'OUTPUT':'TEMPORARY_OUTPUT'})
+                parts = line.strip().split('|', maxsplit=4)
+                cmd = parts[4]
+                if data_dir_regex.search(cmd):
+                    date = parts[2]
+                    cmd2 = data_dir_regex.sub(data_dir_replace_pattern, cmd)
+                    cmd2_quoted = re.sub(r'(?<!\\)"','\\"', cmd2)
+                    print(f"Adding command: {cmd2_quoted}")
+                    output.write(f'    # {date}\n')
+                    output.write(f'    print("Running command: {cmd2_quoted}")\n')
+                    output.write(f'    {cmd2}\n')
+                    added_line = True
 
-if not added_line:
-    print(f"No lines matched {data_dir}.")
-    sys.exit(3)
+    if not added_line:
+        print(f"No lines matched {data_dir}.")
+        sys.exit(3)
 
-print(f"Written commands to {output_file}")
+    print(f"Written commands to {output_file}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("No input data dir specified - exiting")
+        sys.exit(1)
+
+    data_dir = sys.argv[1]
+    qgis_user_profile_dir = None
+
+    if len(sys.argv) > 2:
+        qgis_user_profile_dir = os.path.realpath(sys.argv[2])
+
+    convert_history_to_python(data_dir, qgis_user_profile_dir)
